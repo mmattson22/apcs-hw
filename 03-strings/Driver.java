321543c0eaf63ca2a6209865e6279a09a8038fb2
Michael Mattson <michael@mmattson.com>
2014-09-21 14:04:45 -0400

    string hw

--- 03-strings/Driver.java
+++ 03-strings/Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
     public static void main(String[] args){
-        String s="Michael Mattson";
+        String s="michael mattson";
 	System.out.println("The space is at position: " + s.indexOf(" "));
 	String a=s.substring(0,7);
 	System.out.println("The first name is: " + a);
