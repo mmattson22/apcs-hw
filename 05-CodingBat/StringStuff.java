c0aa0f48274b70ff7ecbb11fb1d9be497694c97f
Michael Mattson <michael@mmattson.com>
2014-09-23 18:12:51 -0400

    codingbat hw

---
+++ 05-CodingBat/StringStuff.java
@@ -0,0 +1,25 @@
+
+
+
+public class StringStuff{
+public String nonStart(String a, String b) {
+  String x = a.substring(1);
+  String y = b.substring(1);
+  return x+y;
+}
+
+
+public String makeAbba(String a, String b) {
+  return a+b+b+a;
+}
+
+public int diff21(int n) {
+  if (n>21) {
+  return ((n-21)*2);
+  } else {
+  return (21-n);
+  }
+}
+}
+
+
