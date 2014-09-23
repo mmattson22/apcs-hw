72f2da90abe0dababd63ef6cbe9b13c79080f5e8
Michael Mattson <michael@mmattson.com>
2014-09-23 18:12:03 -0400

    codingbat hw

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+	public static void main(String[] args){
+		StringStuff s1 = new StringStuff();
+		System.out.println(s1.nonStart("alpha", "bet"));
+		System.out.println(s1.makeAbba("Hi", "Bye"));
+		System.out.println(s1.diff21(19));
+}
+}
