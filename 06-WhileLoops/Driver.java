168faf6ae97ef9bdab361c7b1d972a9e3aa0b1dd
Michael Mattson <michael@mmattson.com>
2014-09-30 19:28:13 -0400

    While loop practice

---
+++ 06-WhileLoops/Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+	public static void main(String[] args){
+		Loops s1 = new Loops();
+		System.out.println(s1.frontTimes("alpha", 3));
+		System.out.println(s1.stringBits("Hello"));
+		System.out.println(s1.stringYak("YakPakYak"));
+		System.out.println(s1.stringMatch("Adioos", "Amigos"));	
+}
+}
---
+++ 06-WhileLoops/Loops.java
@@ -0,0 +1,64 @@
+public class Loops{
+public String frontTimes(String str, int n) {
+  int f = 3;
+  int x = 0;
+  if (f > str.length()) {
+    f = str.length();
+  }
+  String front = str.substring(0, f);
+  
+  String ans = "";
+  while(x<n) {
+    ans = ans + front;
+    x=x+1;
+  }
+  return ans;
+}
+
+
+
+public String stringBits(String str) {
+
+String ans = "";
+int c = 0;
+while(c<str.length()){
+ ans = ans + str.substring(c, c+1);
+ c=c+2;
+ }
+ return ans;
+  
+}
+
+
+public String stringYak(String str) {
+  String a = "";
+  int i = 0;
+  while(i<str.length()) {
+    if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+      i =  i + 3;
+    } else {
+      a = a + str.charAt(i);
+      i=i+1;
+    }
+  }
+  
+  return a;
+}
+
+public int stringMatch(String a, String b) {
+  int count = 0;
+  int l = 0;
+  
+  while (l+1<(a.length())&&l+1<(b.length())){
+    String x = a.substring(l, l+2);
+    String y = b.substring(l, l+2);
+    if (x.equals(y)) {
+      count = count + 1;
+      
+    }
+    l=l+1;
+  }
+
+  return count;
+}
+}
