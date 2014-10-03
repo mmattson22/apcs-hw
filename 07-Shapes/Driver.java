7f60b940a9bd501bb13201fdcc9ed1a6cbca79e3
Michael Mattson <michael@mmattson.com>
2014-10-02 20:46:50 -0400

    added more shape methods

--- 07-Shapes/Driver.java
+++ 07-Shapes/Driver.java
@@ -1,8 +1,14 @@
 public class Driver {
 	public static void main(String[] args){
 	Shapes s1=new Shapes();
-	System.out.println(s1.tri1(5));
-	System.out.println(s1.box(5,5));
-	System.out.println(s1.tri2(5));
+	//System.out.println(s1.tri1(5));
+	//System.out.println(s1.box(5,5));
+	//System.out.println(s1.tri2(5));
+	//System.out.println(s1.tri2b(5));
+	System.out.println(s1.tri3(5));
+	System.out.println(s1.tri4(5));
+	System.out.println(s1.diamond(7));
+	
 }
+
 }
--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -34,7 +34,7 @@ public class Shapes {
 }
 //30 minutes Now works.
   public String tri2(int h){
-        String ans = new String();
+       String ans = new String();
         int row = 1;
         while (row <= h) {
         int i = 0;
@@ -53,5 +53,93 @@ public class Shapes {
         return ans;
     }
 
+public String tri2b(int height){
+        int h;
+	int i;
+	String s="";
+	for(h=1;h<=height;h++){
+	    for(i=0;i<height-h-1;i++){
+		s=s+".";
+	    }
+	    for(i=0;i<h;i++){
+		s=s+"*";
+	    }
+
+
+	s=s+"\n";
+    }
+	return s;
 }
+//10 min
+   public String tri3(int h){
+	String org = "";
+	int current = 0;
+	//current = 1 + current*2
+	while(current < h){
+	    int space;
+	    for(space=0;space<h-current-1;space++){
+		org = org + " ";
+	    }
+	    for( ;space<h+current;space++){
+		org = org + "*";
+	    }
+	    current = current + 1;
+	    org = org + "\n";
+	}
+
+	return org;
+}
+//15 min
+	public String tri4(int h){
+		String ans = new String();
+        	int row=0;
+		int spot = 0;
+        	for (row = 0; row < h; row++) {
+                for (spot = 0; spot < h; spot++) {
+		String layer = "";
+		    if (spot<row){
+			layer += " ";
+			} else {
+			 layer += "*";}
+                    ans += layer;
+                }
+                ans += "\n";
+        } 
+        return ans;
+    }
+//30 min
+	public String diamond(int h){
+		String ans = "";
+		int size = h;
+		int x = 1;
+		int spaceNum=(h/2);
+		String spaces = "                                                                                                               ";
+		for (int i=1;i<=size;i++){
+		  for (int j=spaceNum;j>=1;j--){
+		    ans = ans + " ";
+		  }
+		  for (int j=1;j<=x;j++){
+		    ans = ans + "*";
+		  }
+		ans = ans + "\n";
+		if(i<size/2+1){
+		 x = x+2;
+		 spaceNum=spaceNum-1;
+		} else {
+		 x=x-2;
+		 spaceNum=spaceNum+1;
+		}
+	}
+	if (h%2==0){
+	
+	return ans + spaces.substring(0,(h/2))+"*";
+	} else {
+	return ans;
+}
+
+}
+}
+
+
+
 
