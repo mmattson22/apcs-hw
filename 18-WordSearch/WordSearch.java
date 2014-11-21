d7c47335ef1bca4dc7018bed2213a0676f0af66a
Michael Mattson <michael@mmattson.com>
2014-11-20 21:13:10 -0500

    vertical stuff

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -1,16 +1,20 @@
 public class WordSearch {
+    
+  
     private char[][] board;
-    private int[][] directions = {
-	{0,1},   //Horizontal Right
-	{0,-1},  //Horizontal Left
-	{1,0},   //Vertical Up
-	{-1,0},  //Vertical Down
-	{1,1},   //Up Right
-	{1,-1},  //Up Left
-	{-1,-1}, //Down Left
-	{-1,1}   //Down Right
+
+    private int[][] directions = { 
+	{0,1},   
+	{1,1},   
+	{1,0},   
+	{1,-1},  
+	{0,-1},  
+	{-1,-1}, 
+	{-1,0},
+	{-1,1}  
     };
     
+   
     public WordSearch(int r, int c){
 	board = new char[r][c];
 	for (int i = 0; i < board.length; i++) {
@@ -21,8 +25,9 @@ public class WordSearch {
     }
     
     public WordSearch() {
-	this(30,30);
+	this(40,40);
     }
+    
     
     public String toString(){
 	String s = "";
@@ -35,36 +40,41 @@ public class WordSearch {
 	return s;
     }
     
+   
     public void addWord(String w, int row, int col, int d){
 	String tester = w;
 	int r = row;
 	int c = col;
-	int x = 0;
-	for (int i = 0; i<w.length() && r>=0 && r<board[0].length && c>=0 && c<board.length; i++) {
-	    if (board[r][c] == tester.charAt(i) || board[r][c] =='.') {
-		
+	for (int i = 0; i<w.length(); i++) {
+	    if (r>=0 && r<board[0].length && c>=0 && c<board.length && (board[r][c] == tester.charAt(i) || board[r][c] =='.')) {
+		r += directions[d][0];
+		c += directions[d][1];
+	    } else {
+		System.out.println("Can't put word here");
+		System.exit(0);
+	    }
+	}
+	
+	r = row;
+	c = col;
+	
+	for (int i = 0; i<w.length(); i++) {
+	    board[r][c] = w.charAt(i);
 	    r += directions[d][0];
 	    c += directions[d][1];
-	}else{
-		x++;
+	}
     }
-  }}
 
-	public static void main(String[] args){
+  
+    public static void main(String[] args) {
 	WordSearch w = new WordSearch();
+	
+	w.addWord("hello",3,15,0); // should work
 	System.out.println(w);
-            /*
-	      w.addWordH("hello",3,15); // should work
-	      w.addWordH("look",3,14); // test illegal overlap
-	      //w.addWordH("look",3,18); // test legal overlap
-	      //w.addWordH("look",-3,20); // test illegal row
-	      //w.addWordH("look",3,55); // test illegal col
-	      // etc
-	      */
+	
+	w.addWord("this",23,10,2);
+	
+	w.addWord("cool",27,10,2);
 	System.out.println(w);
     }
-
 }
-
-
-   
